63c8b19fbee0f5b614d0b8e47e5691fb48aed361
john whitley <jmlove5683@gmail.com>
2024-03-14 11:20:56 -0600

    added part to of excersise to assignment

--- level2/assignments/sp108/main.js
+++ level2/assignments/sp108/main.js
@@ -1,4 +1,10 @@
 const arr = [2, 4, 6, 8, 10]
-const result = arr.map(num => num * 2)
+const number = arr.map(num => num * 2)
 
-console.log(result)
+console.log(number)
+
+const array = [1, 3, 5, 7, 9]
+const stringItUp = arr.map(function(stringItUp){
+    return stringItUp.map
+})
+
